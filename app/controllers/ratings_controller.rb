5ffaf32e3bb3248f582bce9f5b60c9002e5a8fea
damiansliwa <damian.sliwa.90@gmail.com>
2019-01-02 21:03:53 +0100

    Improve RatingsController and User model for better readability

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,10 +2,8 @@ class RatingsController < ApplicationController
   def index
   	commenters_count = 10
   	days_range = 7
-  	@users_with_most_comments = User.joins(:comments)
-  									.group('comments.user_id')
+  	@users_with_most_comments = User.by_comments_count
   									.having('comments.created_at >= ?', days_range.days.ago)
-  									.order('COUNT(comments.id) DESC')
   									.limit(commenters_count)
   end
 end
