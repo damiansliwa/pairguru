6aba9446d9c407d4b80d5510759bcda18a2fe75e
damiansliwa <damian.sliwa.90@gmail.com>
2019-01-02 12:31:30 +0100

    Improve RatingsController to increase it's flexibility

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,9 +1,11 @@
 class RatingsController < ApplicationController
   def index
+  	commenters_count = 10
+  	days_range = 7
   	@users_with_most_comments = User.joins(:comments)
   									.group('comments.user_id')
-  									.having('comments.created_at >= ?', 7.days.ago)
+  									.having('comments.created_at >= ?', days_range.days.ago)
   									.order('COUNT(comments.id) DESC')
-  									.limit(10)
+  									.limit(commenters_count)
   end
 end
