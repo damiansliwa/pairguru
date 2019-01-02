5362767ec327255049f5d6dfe0a89b11b0158856
damiansliwa <damian.sliwa.90@gmail.com>
2019-01-02 12:19:46 +0100

    Create controllre and view for rewarded users

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,9 @@
 class RatingsController < ApplicationController
+  def index
+  	@users_with_most_comments = User.joins(:comments)
+  									.group('comments.user_id')
+  									.having('comments.created_at >= ?', 7.days.ago)
+  									.order('COUNT(comments.id) DESC')
+  									.limit(10)
+  end
 end
